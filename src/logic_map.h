4dd9f98d149a1e38c9ff1f2a477e6d692bb41a40
Robin Volf <xvolfr00@stud.fit.vutbr.cz>
2023-05-02 12:31:59 +0200

    Přidání interní log. reprezentace mapy

---
+++ src/logic_map.h
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include "map_item.h"
+#include <iostream>
+
+//makro pro indexování do mapy
+#define INDEX(x, y) x + this->width * y
+
+/**
+Třída pro zachycení logické reprezentace mapy, užitečné pro hledání cest (pathfinding)
+*/
+class Logical_map {
+    private:
+    size_t width;
+    size_t height;
+    size_t index(size_t x, size_t y);
+    map_item_type* map;
+
+    public:    
+    Logical_map();
+    ~Logical_map();
+    void new_map(size_t width, size_t height);
+    void set_tile(size_t x, size_t y, map_item_type tile_type);
+    map_item_type get_tile(size_t x, size_t y);
+    void pathfind_bfs();
+    void get_random_path();
+    void print_map();
+};
