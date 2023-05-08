ea678c38e3943b3805b26717065e49404ebb558d
Patrik Uher <patrikuher35@gmail.com>
2023-05-08 22:48:00 +0200

    Zmena main na vyuziti menu

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,15 @@
 #include <QGraphicsView>
 #include <cstdlib>
 #include <stdexcept>
-#include "game.h"
+#include "main_ui.h"
 
 int main(int argc, char* argv[]) {
     srand((unsigned int)time(0));
 
     QApplication app(argc, argv);
-
-    // Game* my_game;
-    Replay* my_game;
-    try{
-        my_game = new Replay("logfile.xml");
-        // my_game = new Game("map.txt", "logfile.xml");
-
-    } catch(std::runtime_error& e) {
-        qDebug() << e.what();
-        return EXIT_FAILURE;
-    }
-
-    my_game->initialize_entities();
-    my_game->start();
-
-    QGraphicsView view(my_game);
-    view.show();
     
-    app.exec();
-    delete my_game;
-    return EXIT_SUCCESS;
+    Ui_MainWindow mwin = Ui_MainWindow(&app);
+    mwin.MainWindow->show();
+
+    return app.exec();
 }
